e0e3c95cc5261a107ff91efbf79ddbd35fcd1d20
John UngJae Yun <45244448+ungjae@users.noreply.github.com>
2025-02-22 16:53:17 -0500

    Revert "update start.sh to use sudo to fix permission errors"
    This reverts commit 985af30dd8bc2426989e613d8f13e68fc10b6138.

--- start.sh
+++ start.sh
@@ -2,18 +2,17 @@
 
 echo "starting Receipt Processor"
 
-sudo docker compose down
+docker compose down
 
-sudo docker compose up --build -d
+docker compose up --build -d
 
 echo
 echo "wait 5 seconds for containers to initialize"
 sleep 5
 
-sudo docker compose ps
+docker compose ps
 
 echo
 echo "showing logs"
 echo
-sudo docker compose logs -f
-echo "bye"
+docker compose logs -f
