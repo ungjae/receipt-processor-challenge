985af30dd8bc2426989e613d8f13e68fc10b6138
John UngJae Yun <45244448+ungjae@users.noreply.github.com>
2025-02-22 16:16:59 -0500

    update start.sh to use sudo to fix permission errors

--- start.sh
+++ start.sh
@@ -2,17 +2,18 @@
 
 echo "starting Receipt Processor"
 
-docker compose down
+sudo docker compose down
 
-docker compose up --build -d
+sudo docker compose up --build -d
 
 echo
 echo "wait 5 seconds for containers to initialize"
 sleep 5
 
-docker compose ps
+sudo docker compose ps
 
 echo
 echo "showing logs"
 echo
-docker compose logs -f
+sudo docker compose logs -f
+echo "bye"
